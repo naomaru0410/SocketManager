312c5af964eb5ba3c7ffb61a75b27461b9c41395
oshiba <oshiba@1-10.com>
2017-05-26 18:06:36 +0900

    works詳細をsort

---
+++ SocketManager/SocketManager/SocketManager.h
@@ -0,0 +1,19 @@
+//
+//  SocketManager.h
+//  SocketManager
+//
+//  Created by oshiba on 2017/05/10.
+//  Copyright © 2017年 oshiba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SocketManager.
+FOUNDATION_EXPORT double SocketManagerVersionNumber;
+
+//! Project version string for SocketManager.
+FOUNDATION_EXPORT const unsigned char SocketManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SocketManager/PublicHeader.h>
+
+
